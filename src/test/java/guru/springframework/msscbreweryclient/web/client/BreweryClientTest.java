f592627424e54af5f173f9d94b20747a9e13c06b
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-25 16:36:51 +0600

    HTTP PUT with Spring RestTemplate

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -32,4 +32,9 @@ public class BreweryClientTest {
 		System.out.println(uri.toString());
 	}
 
+	@Test
+	void testUpdateBeer(){
+		BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+		client.updateBeer(UUID.randomUUID(), beerDto);
+	}
 }
